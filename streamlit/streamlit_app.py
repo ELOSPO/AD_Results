f71a559db32dabb9043ef5e2d425a6812623e7bb
ELOSPO <elospieconomics@gmail.com>
2025-02-13 07:12:21 -0500

    feature(main): Create buttom and top portfolios

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -54,8 +54,41 @@ def main():
 # Display the chart in Streamlit
     st.altair_chart(chart, use_container_width=True)
     st.subheader(f'Model MAPE for {stock} in test is {round(df_mape[stock].iloc[0]*100,3)}%')
-    st.write('Disclaimer: Invest in capital markets is a risky way to make money, Do not Invest money you need I WILL BE NOT RESPONSIBLE FOR YOUR ACTIONS ON FINANCIAL MARKETS')
+    
     # print(df_pred[stock])
+    performances = []
+    erros = []
+
+    list_columns = df_pred.columns.tolist()[:-2]
+    
+    for symbol in list_columns:
+        performance = (df_pred[f'{symbol}'].iloc[-1] - df_pred[f'{symbol}'].iloc[0])/df_pred[f'{symbol}'].iloc[0]
+        performances.append(performance)
+        error = df_mape[f'{symbol}'].iloc[0]
+        erros.append(error)
+
+    st.header(f'Expected Top and Buttom Performers')
+    ln_val = st.slider('Select the number of stocks in the top and buttom',1,20,1)
+    df_perf = pd.DataFrame(zip(list_columns,performances,erros),columns=['asset','expected_performance','error'])
+    df_perf = df_perf.set_index('asset')
+    df_perf = df_perf.sort_values('expected_performance',ascending=False)
+    df_perf2 = df_perf.copy()
+    df_perf2 = df_perf2[df_perf2['error'] <0.1]
+    top_5 = df_perf2.head(ln_val)
+    bottom_5 = df_perf2.tail(ln_val)
+
+    col1_top, col2_top = st.columns(2)    
+    
+    with col1_top:
+        st.subheader(f'Top {ln_val}')
+        st.dataframe(top_5)
+    with col2_top:
+        st.subheader(f'Buttom {ln_val}')
+        st.dataframe(bottom_5)
+        
+
+    
+    st.write('Disclaimer: Invest in capital markets is a risky way to make money, Do not Invest money you need. I WILL BE NOT RESPONSIBLE FOR YOUR ACTIONS ON FINANCIAL MARKETS')
 
 if __name__ == '__main__':
     main()
